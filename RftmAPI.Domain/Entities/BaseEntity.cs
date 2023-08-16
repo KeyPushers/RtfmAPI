7acfdac259b6aba9c0a924282f298bddbf83202d
kassfb <79.kas.98@gmail.com>
2023-08-16 22:30:39 +0300

    feat: Создал базовые сущности

---
+++ RftmAPI.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace RftmAPI.Domain.Entities;
+
+/// <summary>
+/// Базовая сущность
+/// </summary>
+public abstract class BaseEntity
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public Guid Id { get; init; }
+}
---
+++ RftmAPI.Domain/Entities/Genre.cs
@@ -0,0 +1,22 @@
+﻿namespace RftmAPI.Domain.Entities;
+
+/// <summary>
+/// Жанр музыки
+/// </summary>
+public class Genre : BaseEntity
+{
+    /// <summary>
+    /// Название жанра музыки
+    /// </summary>
+    public string? Name { get; init; }
+    
+    /// <summary>
+    /// Музыкальные треки, относящиеся к этому жанру
+    /// </summary>
+    public List<Track> Tracks { get; init; } = new();
+    
+    /// <summary>
+    /// Музыкальные группы, относящиеся к этому жанру
+    /// </summary>
+    public List<Band> Bands { get; init; } = new();
+}
