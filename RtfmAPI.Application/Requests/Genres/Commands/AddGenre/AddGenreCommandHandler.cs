3da8d9022ca8f7ce9361ed4f3da7598fbcf44f5a
Pavel <besedinskiy@tecon.ru>
2024-03-06 22:23:31 +0300

    feat: Добавление обработчика команды добавления музыкального жанра

---
+++ RtfmAPI.Application/Requests/Genres/Commands/AddGenre/AddGenreCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using RtfmAPI.Application.Interfaces.Persistence.Commands;
+using RtfmAPI.Application.Requests.Genres.Commands.AddGenre.Dtos;
+using RtfmAPI.Domain.Models.Genres;
+using RtfmAPI.Domain.Models.Genres.ValueObjects;
+using RtfmAPI.Domain.Primitives;
+
+namespace RtfmAPI.Application.Requests.Genres.Commands.AddGenre;
+
+/// <summary>
+/// Обработчик команды добавления музыкального жанра.
+/// </summary>
+public class AddGenreCommandHandler : IRequestHandler<AddGenreCommand, Result<AddedGenre>>
+{
+    private readonly IGenresCommandsRepository _repository;
+
+    /// <summary>
+    /// Создание обработчика команды добавления музыкального жанра.
+    /// </summary>
+    /// <param name="repository">Репозиторий команд музыкального жанра.</param>
+    public AddGenreCommandHandler(IGenresCommandsRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    /// <summary>
+    /// Обработка команды добавления музыкального жанра.
+    /// </summary>
+    /// <param name="request">Команда добавления музыкального жанра.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Объект переноса данных добавленного жанра.</returns>
+    public async Task<Result<AddedGenre>> Handle(AddGenreCommand request, CancellationToken cancellationToken = default)
+    {
+        // TODO: Добавить проверку существования музыкального жанра с тем же именем.
+
+        var createGenreNameResult = GenreName.Create(request.Name);
+        if (createGenreNameResult.IsFailed)
+        {
+            return createGenreNameResult.Error;
+        }
+
+        var genreName = createGenreNameResult.Value;
+
+        var createGenreResult = Genre.Create(genreName);
+        if (createGenreNameResult.IsFailed)
+        {
+            return createGenreResult.Error;
+        }
+
+        var genre = createGenreResult.Value;
+        await _repository.CommitChangesAsync(genre);
+        
+        return new AddedGenre
+        {
+            Id = genre.Id.Value,
+            Name = genre.Name.Value
+        };
+    }
+}
---
+++ RtfmAPI.Application/Requests/Genres/Commands/AddGenre/Dtos/AddingGenre.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RtfmAPI.Application.Requests.Genres.Commands.AddGenre.Dtos;
+
+/// <summary>
+/// Объект переноса данных добавляемого музыкального жанра.
+/// </summary>
+public sealed class AddingGenre
+{
+    /// <summary>
+    /// Название музыкального жанра.
+    /// </summary>
+    [Required]
+    public string? Name { get; init; }
+}
