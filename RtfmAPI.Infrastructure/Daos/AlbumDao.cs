0c85d0bbd7f1712093dfba6d210cb68177da3dd0
Pavel <besedinskiy@tecon.ru>
2024-03-08 17:52:30 +0300

    feat: Добавлены комментарии к объектам доступа данных

--- RtfmAPI.Infrastructure/Daos/AlbumDao.cs
+++ RtfmAPI.Infrastructure/Daos/AlbumDao.cs
@@ -2,9 +2,23 @@
 
 namespace RtfmAPI.Infrastructure.Daos;
 
+/// <summary>
+/// Объект доступа данных музыкального альбома.
+/// </summary>
 public class AlbumDao
 {
+    /// <summary>
+    /// Идентификатор музыкального альбома.
+    /// </summary>
     public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Название музыкального альбома.
+    /// </summary>
     public string Name { get; set; } = null!;
+    
+    /// <summary>
+    /// Дата выпуска музыкального альбома.
+    /// </summary>
     public DateTime ReleaseDate { get; set; }
 }
--- RtfmAPI.Infrastructure/Daos/GenreDao.cs
+++ RtfmAPI.Infrastructure/Daos/GenreDao.cs
@@ -2,8 +2,18 @@
 
 namespace RtfmAPI.Infrastructure.Daos;
 
+/// <summary>
+/// Объект доступа данных музыкального жанра.
+/// </summary>
 public class GenreDao
 {
+    /// <summary>
+    /// Идентификатор музыкального жанра.
+    /// </summary>
     public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Название музыкального жанра.
+    /// </summary>
     public string Name { get; set; } = null!;
 }
