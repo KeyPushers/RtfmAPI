e7c210356f7027cabe5655814b8189581c77e3ad
Pavel <besedinskiy@tecon.ru>
2024-03-08 17:52:52 +0300

    feat: Добавлен объект доступа данных музыкального трека

---
+++ RtfmAPI.Infrastructure/Daos/TrackDao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RtfmAPI.Infrastructure.Daos;
+
+/// <summary>
+/// Объект доступа данных музыкального трека.
+/// </summary>
+public class TrackDao
+{
+    /// <summary>
+    /// Идентификатор музыкального трека.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Название музыкального трека.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Идентификатор файла музыкального трека.
+    /// </summary>
+    public Guid? TrackFileId { get; set; }
+
+    /// <summary>
+    /// Идентификаторы музыкальных жанров.
+    /// </summary>
+    public List<Guid> GenreIds { get; set; } = new();
+}
