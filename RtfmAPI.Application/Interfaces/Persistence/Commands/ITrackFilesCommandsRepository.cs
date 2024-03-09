f8bbe895faad58669516f38fe326eda8fb04471c
Pavel Besedinsky <pavel_besedinck@mail.ru>
2024-03-09 14:57:45 +0300

    feat: Добавлен репозиторий команд файла музыкального трека

---
+++ RtfmAPI.Application/Interfaces/Persistence/Commands/ITrackFilesCommandsRepository.cs
@@ -0,0 +1,12 @@
+﻿using RtfmAPI.Application.Interfaces.Persistence.Common;
+using RtfmAPI.Domain.Models.TrackFiles;
+
+namespace RtfmAPI.Application.Interfaces.Persistence.Commands;
+
+/// <summary>
+/// Интерфейс репозитория комманд доменной модели <see cref="TrackFile"/>.
+/// </summary>
+public interface ITrackFilesCommandsRepository : ICommit<TrackFile>
+{
+    
+}
